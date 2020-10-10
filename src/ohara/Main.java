762a8594816d3081346fbb55efccfa426a78b8c4
Irina Ohara <irina.v.ohara@gmail.com>
2020-10-10 11:39:04 -0600

    Initial commit

---
+++ src/ohara/Main.java
@@ -0,0 +1,8 @@
+package ohara;
+
+public class Main {
+    public static void main(String[] args){
+        String name = "Irina";
+        System.out.println( "Hello, my name is " + name);
+    }
+}
