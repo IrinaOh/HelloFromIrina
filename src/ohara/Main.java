fddbee5e2723d321e4bbc76e1151f0b191a50b1d
Thomas Weaver <tjayweaver@gmail.com>
2020-10-10 14:27:51 -0400

    edits from Thomas

--- src/ohara/Main.java
+++ src/ohara/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args){
         String name = "Irina";
         System.out.println( "Hello, my name is " + name);
+        System.out.println("Hello Irina, my name is Thomas");
     }
 }
